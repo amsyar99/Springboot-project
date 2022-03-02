5edbe1a6b928e16dc0c9bc0939484e41872f24ef
amsyar99 <amsyargy@gmai.com>
2022-03-03 01:39:49 +0800

    Create Post Rest API

--- src/main/java/com/springboot/blog/payload/PostDto.java
+++ src/main/java/com/springboot/blog/payload/PostDto.java
@@ -1,2 +1,11 @@
-package com.springboot.blog.payload;public class PostDto {
+package com.springboot.blog.payload;
+
+import lombok.Data;
+
+@Data
+public class PostDto {
+    private long id;
+    private String title;
+    private String description;
+    private String content;
 }
