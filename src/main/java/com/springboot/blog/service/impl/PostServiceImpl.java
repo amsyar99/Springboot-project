e242d1436658e67621410feb5e4ced42ef9946f2
amsyar99 <amsyargy@gmail.com>
2022-03-08 13:20:56 +0800

    update

--- src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
+++ src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 @Service
