5821717aca9846e84dfd9cb92d62e7d33fa64dfb
amsyar99 <amsyargy@gmail.com>
2022-05-05 01:34:43 +0800

    Add not null and size annotation in post DTO class

--- src/main/java/com/springboot/blog/payload/PostDto.java
+++ src/main/java/com/springboot/blog/payload/PostDto.java
@@ -1,14 +1,26 @@
 package com.springboot.blog.payload;
 
+import com.sun.istack.NotNull;
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.Set;
 
 @Data
 public class PostDto {
     private long id;
+
+    @NotEmpty
+    @Size(min = 2, message = "Title should not less than 2 character")
     private String title;
+    @NotEmpty
+    @Size(min = 2, message = "Title should not less than 2 character")
     private String description;
+    @NotEmpty
+    @Size(min = 2, message = "Title should not less than 2 character")
     private String content;
+    @NotEmpty
+    @Size(min = 2, message = "Title should not less than 2 character")
     private Set<CommentDto> comments;
 }
