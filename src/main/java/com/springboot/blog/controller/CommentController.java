10ddce57b03f8bfd018819c1a3086bcbe376ec90
amsyar99 <amsyargy@gmail.com>
2022-05-05 02:30:00 +0800

    Validate comment DTO code

--- src/main/java/com/springboot/blog/controller/CommentController.java
+++ src/main/java/com/springboot/blog/controller/CommentController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -22,7 +23,7 @@ public class CommentController {
 
     @PostMapping("/posts/{postId}/comments")
     public ResponseEntity<CommentDto> createComment(@PathVariable(value = "postId") long postId,
-                                                    @RequestBody CommentDto commentDto){
+                                                    @Valid @RequestBody CommentDto commentDto){
     return new ResponseEntity<>(commentService.createComment(postId,commentDto), HttpStatus.CREATED);
     }
     @GetMapping("/posts/{postId}/comments")
--- src/main/java/com/springboot/blog/payload/CommentDto.java
+++ src/main/java/com/springboot/blog/payload/CommentDto.java
@@ -2,14 +2,23 @@ package com.springboot.blog.payload;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.List;
 import java.util.stream.Collector;
 
 @Data
 public class CommentDto {
     private long id;
+    @NotEmpty(message = "Name should not be empty")
     private String name;
+
+    @NotEmpty(message = "Email should not be empty")
+    @Email
     private String email;
+    @NotEmpty
+    @Size(min = 10, message = "Comment body should not less than 10 character")
     private String body;
 
 }
